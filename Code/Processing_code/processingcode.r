4751865a6bd3bfc2c61a6198d2ac25425f7a49ea
cusisom <danielcu@hawaii.edu>
2023-04-27 02:23:34 -1000

    processed

--- Code/Processing_code/processingcode.r
+++ Code/Processing_code/processingcode.r
@@ -113,6 +113,53 @@ d2 <- dat1 %>% relocate(where(is.factor), .before = Xalarl)
 
 kable(d2[1:10, ])
 
+## ---- d1Array --------
+
+#I need to organize my data into a format that works for geomorph
+#I start by subsetting the data without the demographic information
+
+land1 <- d1[,-c(1:3)]
+
+#In making my array, I will still want each individual sheet to be associated with the specimen IDs instead of numeric rownames
+#Check rownames
+rownames(land1)
+
+#Change rownames to specimen IDs
+rownames(land2) <- d2$ID
+
+head(land2)
+
+#Next step is to transform my data into an array
+#I need to determine how many landmarks I have so I can set the parameters
+
+dim(land1)
+
+#this tells me that my data is comprised of 92 individual specimens, each with and associated 114 landmark measurements
+#I know that each landmark is given x, y, and z data
+#I will want my array to be comprised of 92 sheets containing x, y, and z data for each landmark
+#I then need to determine the number of landmarks
+#Noting that there are three data points for each I can divide the total measure by 3 to get the total landmarks
+
+my_mat <- apply(as.matrix.noquote(land1), 2, as.numeric)
+my_mat
+
+#From this I can develop an array for my land2 data (92 specimens) that has 38 landmarks with 3 measures per landmark
+
+d1array <- arrayspecs(my_mat, 38, 3)
+
+
+
+#I want to check my data but I don't want it to take up too much space
+#I can index the first couple of sheets to see the layout
+
+str(d1array)
+
+save_data_location_csv <- "../../Data/Processed_data/Japan_array.csv"
+write.csv(d1array, file = save_data_location_csv, row.names=FALSE)
+
+save_data_location <- "../../Data/Processed_data/Japan_array.rds"
+saveRDS(d1array, file = save_data_location)
+
 ## ---- d2Array --------
 
 #I need to organize my data into a format that works for geomorph
@@ -144,6 +191,12 @@ dim(land2)
 
 #From this I can develop an array for my land2 data (92 specimens) that has 38 landmarks with 3 measures per landmark
 
+my_mat2 <- apply(as.matrix.noquote(land2), 2, as.numeric)
+my_mat2
+
+#From this I can develop an array for my land2 data (92 specimens) that has 38 landmarks with 3 measures per landmark
+
+d1array <- arrayspecs(my_mat2, 38, 3)
 d2array <- arrayspecs(land2, 38, 3)
 
 #I want to check my data but I don't want it to take up too much space
@@ -152,13 +205,21 @@ d2array <- arrayspecs(land2, 38, 3)
 d2array[1:4, , 1:2]
 
 #I'll also make sure that my number of landmarks per specimen is correct
-#For that I will skim the first few sheets
 
-skimd2 <- skim(d2array)
+str(d2array)
 
-head(skimd2)
+save_data_location_csv <- "../../Data/Processed_data/Korea_array.csv"
+write.csv(d2array, file = save_data_location_csv, row.names=FALSE)
 
 #From here it looks like d2 is ready for analysis
 #d2 is the complete dataset for Korea
 #d2array is the needed landmark data without the population and sex information in an array structure
-#skimd2 is the skim for the complete array
+
+
+
+save_data_location_csv <- "../../Data/Processed_data/Korea_array.csv"
+write.csv(d2array, file = save_data_location_csv, row.names=FALSE)
+
+save_data_location <- "../../Data/Processed_data/Korea_array.rds"
+saveRDS(d2array, file = save_data_location)
+
